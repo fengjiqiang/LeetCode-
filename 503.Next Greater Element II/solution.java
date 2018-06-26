562488830009096fdf6f1d1b5b7f98bdb59fb925
Jiqiang Feng <feng1187932281@163.com>
2018-06-26 15:58:10 +0800

    Create solution.java

---
+++ 503.Next Greater Element II/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] next = new int[n];
+        Arrays.fill(next, -1);
+        Stack<Integer> pre = new Stack<>();
+        for (int i = 0; i < n * 2; i++) {
+            int num = nums[i % n];
+            while (!pre.isEmpty() && nums[pre.peek()] < num) {
+                next[pre.pop()] = num;
+            }
+            if (i < n){
+                pre.push(i);
+            }
+        }
+        return next;
+    }
+}
