42cdde77ea9716e6347a84fec2c873481a9ce993
Jiqiang Feng <feng1187932281@163.com>
2018-06-26 15:59:01 +0800

    Update solution.java

--- 503.Next Greater Element II/solution.java
+++ 503.Next Greater Element II/solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
         int[] next = new int[n];
-        Arrays.fill(next, -1);
+        Arrays.fill(next, -1);  //填充数组next中的每一个元素为-1
         Stack<Integer> pre = new Stack<>();
         for (int i = 0; i < n * 2; i++) {
             int num = nums[i % n];
