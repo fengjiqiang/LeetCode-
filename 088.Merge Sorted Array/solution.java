b925a54e2e9cc8e6a673ad781ee668aa07e79cd1
Jiqiang Feng <feng1187932281@163.com>
2018-06-28 19:42:05 +0800

    Create solution.java

---
+++ 088.Merge Sorted Array/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, writeIdx = m + n - 1;
+        while (i >= 0 && j >= 0)
+            nums1[writeIdx--] = nums1[i] > nums2[j]? nums1[i--] : nums2[j--];
+        while (j >= 0)
+            nums1[writeIdx--] = nums2[j--];
+    }
+}
