617a7614ecac05812ac42665654184cc1ea43fd1
Jiqiang Feng <feng1187932281@163.com>
2018-07-26 20:00:37 +0800

    Create Solution2.java

---
+++ 104.Maximum Depth of Binary Tree/Solution2.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        // 非递归
+        if(root==null)
+            return 0;
+        int res = 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            ++res;
+            int n = q.size();
+            for (int i = 0; i < n; ++i) {
+                TreeNode t = q.poll();
+                if (t.left != null) q.offer(t.left);
+                if (t.right != null) q.offer(t.right);
+            }
+        }
+        return res;
+    }
+}
